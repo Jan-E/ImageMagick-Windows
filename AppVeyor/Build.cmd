d832cc21cd70a0262257b0e2c905a909f8c10637
Jan-E <github@ehrhardt.nl>
2018-02-03 19:01:17 +0100

    Visual Studio 2017 build deps vc15/vc14 + src

--- AppVeyor/Build.cmd
+++ AppVeyor/Build.cmd
@@ -1,4 +1,8 @@
 @echo off
-call "%vs120comntools%vsvars32.bat"
+if not "%3"=="VS2017" call "%vs140comntools%vsvars32.bat"
+if     "%3"=="VS2017" call "C:\Program Files (x86)\Microsoft Visual Studio\2017\Community\VC\Auxiliary\Build\vcvars32.bat"
 
-powershell -ExecutionPolicy Unrestricted .\Build.ps1 %1 %2
+if not "%3"=="VS2017" set include=%include%;"C:\Program Files (x86)\Microsoft Visual Studio 14.0\VC\atlmfc\include"
+if     "%3"=="VS2017" set include=%include%;"C:\Program Files (x86)\Microsoft Visual Studio\2017\Community\VC\Tools\MSVC\14.12.25827\atlmfc\include"
+
+powershell -ExecutionPolicy Unrestricted .\Build.ps1 %1 %2 %3 %4
