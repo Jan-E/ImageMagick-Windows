3ea1c3cdb6b4b01430d6e51bfc5d89e9867527fd
Jan-E <github@ehrhardt.nl>
2018-02-04 04:02:13 +0100

    vc11 x86 call C:\Program Files (x86)\Microsoft Visual Studio 12.0\Common7\Tools\vsvars32.bat

--- AppVeyor/Build.cmd
+++ AppVeyor/Build.cmd
@@ -1,5 +1,5 @@
 @echo off
 rem "C:\Program Files (x86)\Microsoft Visual Studio 11.0\VC\vcvarsall.bat" x86
-    "%vs120comntools%vsvars32.bat"
+    call "%vs120comntools%vsvars32.bat"
 
 powershell -ExecutionPolicy Unrestricted .\Build.ps1 %1 %2 %3 %4
