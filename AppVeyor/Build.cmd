6d2ebc438a8a84001cae3e87033cb678776b0d13
Jan-E <github@ehrhardt.nl>
2018-02-04 03:38:52 +0100

    vc11 x86

--- AppVeyor/Build.cmd
+++ AppVeyor/Build.cmd
@@ -1,8 +1,5 @@
 @echo off
-if not "%3"=="VS2017" call "%vs140comntools%vsvars32.bat"
-if     "%3"=="VS2017" call "C:\Program Files (x86)\Microsoft Visual Studio\2017\Community\VC\Auxiliary\Build\vcvars32.bat"
-
-if not "%3"=="VS2017" set include=%include%;"C:\Program Files (x86)\Microsoft Visual Studio 14.0\VC\atlmfc\include"
-if     "%3"=="VS2017" set include=%include%;"C:\Program Files (x86)\Microsoft Visual Studio\2017\Community\VC\Tools\MSVC\14.12.25827\atlmfc\include"
+if not "%3"=="VS2013" call "C:\Program Files (x86)\Microsoft Visual Studio 11.0\VC\vcvarsall.bat" x86
+if     "%3"=="VS2013" call "%vs140comntools%vsvars32.bat"
 
 powershell -ExecutionPolicy Unrestricted .\Build.ps1 %1 %2 %3 %4
