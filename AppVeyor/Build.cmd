69607124e797649426871907b700192149a81472
Jan-E <github@ehrhardt.nl>
2018-04-19 14:59:37 +0200

    include afxwin.h

--- AppVeyor/Build.cmd
+++ AppVeyor/Build.cmd
@@ -2,7 +2,8 @@
 if not "%3"=="VS2017" call "%vs140comntools%vsvars32.bat"
 if     "%3"=="VS2017" call "C:\Program Files (x86)\Microsoft Visual Studio\2017\Community\VC\Auxiliary\Build\vcvars32.bat"
 
-if not "%3"=="VS2017" set include=%include%;"C:\Program Files (x86)\Microsoft Visual Studio 14.0\VC\atlmfc\include"
-if     "%3"=="VS2017" set include=%include%;"C:\Program Files (x86)\Microsoft Visual Studio\2017\Community\VC\Tools\MSVC\14.12.25827\atlmfc\include"
+dir "C:\Program Files (x86)\Microsoft Visual Studio 14.0\VC\atlmfc\include\afxwin.h"
+dir "C:\Program Files (x86)\Microsoft Visual Studio\2017\Community\VC\Tools\MSVC\afxwin*.h" /s
+set include=%include%;"C:\Program Files (x86)\Microsoft Visual Studio 14.0\VC\atlmfc\include";"C:\Program Files (x86)\Microsoft Visual Studio\2017\Community\VC\Tools\MSVC\14.13.26128\atlmfc\include"
 
 powershell -ExecutionPolicy Unrestricted .\Build.ps1 %1 %2 %3 %4 %5 %6 %7 %8 %9
