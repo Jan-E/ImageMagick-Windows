94650f031561414524f16527decab23a2fb7accf
Jan-E <github@ehrhardt.nl>
2020-11-23 14:31:01 +0100

    Sync with original clone ImageMagick

--- CloneRepositories.sh
+++ CloneRepositories.sh
@@ -92,7 +92,7 @@ clone_repository $1 $2 'webp'
 clone_repository $1 $2 'win2k'
 clone_repository $1 $2 'zlib'
 
-clone_repository $1 $2 'ImageMagick' '299cb95'
+clone_repository $1 $2 'ImageMagick'
 
 if [ "$2" == "source" ]; then
   for folder in *; do
