744e1789c82e767c45ef127133be8f1cc919df8e
Jan-E <github@ehrhardt.nl>
2021-03-13 14:03:25 +0100

    Build from git HEAD

--- CloneRepositories.sh
+++ CloneRepositories.sh
@@ -95,7 +95,7 @@ clone_repository $1 $2 'webp'
 clone_repository $1 $2 'win2k'
 clone_repository $1 $2 'zlib'
 
-clone_repository $1 $2 'ImageMagick' 'main' 'ImageMagick' '24050e2'
+clone_repository $1 $2 'ImageMagick'
 
 if [ "$2" == "source" ]; then
   for folder in *; do
