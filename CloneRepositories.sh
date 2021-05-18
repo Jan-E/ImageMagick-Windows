c8e019653ceb0d01282559b033079f82a4a9a1ba
Jan-E <github@ehrhardt.nl>
2021-05-18 22:55:16 +0200

    Release 7.0.11-13, build ff86255

--- CloneRepositories.sh
+++ CloneRepositories.sh
@@ -95,7 +95,7 @@ clone_repository $1 $2 'webp'
 clone_repository $1 $2 'win2k'
 clone_repository $1 $2 'zlib'
 
-clone_repository $1 $2 'ImageMagick'
+clone_repository $1 $2 'ImageMagick' 'main' 'ImageMagick' 'ff86255'
 
 if [ "$2" == "source" ]; then
   for folder in *; do
